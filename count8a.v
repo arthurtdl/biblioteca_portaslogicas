11407054f95d602d63f1e20c18c7f2b586ad5bf7
Arthur Torres <arthur.torresdl03@gmail.com>
2025-03-14 10:26:58 -0300

    count8a finalizado e aprovado pelo teste de regressão

--- count8a.v
+++ count8a.v
@@ -1,7 +1,6 @@
-`include "and_gate.v"
 `include "tflipflop.v"
 
-module counta (output wire [7:0] cnt,
+module count8a (output wire [7:0] cnt,
                input wire Clk, Res, En, Load,
                input wire [7:0] cnt_in);
 
@@ -15,13 +14,13 @@ module counta (output wire [7:0] cnt,
   and_gate AND5 (.out(ff5_ff6), .in1(cnt[5]), .in2(ff4_ff5));
   and_gate AND6 (.out(ff6_ff7), .in1(cnt[6]), .in2(ff5_ff6));
 
-  tflipflop T0 (.Q(cnt[0]), .En(En), .T(Clk), .Res(Res), .Pres());
-  tflipflop T1 (.Q(cnt[1]), .En(ff0_ff1), .T(Clk), .Res(Res), .Pres());
-  tflipflop T2 (.Q(cnt[2]), .En(ff1_ff2), .T(Clk), .Res(Res), .Pres());
-  tflipflop T3 (.Q(cnt[3]), .En(ff2_ff3), .T(Clk), .Res(Res), .Pres());
-  tflipflop T4 (.Q(cnt[4]), .En(ff3_ff4), .T(Clk), .Res(Res), .Pres());
-  tflipflop T5 (.Q(cnt[5]), .En(ff4_ff5), .T(Clk), .Res(Res), .Pres());
-  tflipflop T6 (.Q(cnt[6]), .En(ff5_ff6), .T(Clk), .Res(Res), .Pres());
-  tflipflop T7 (.Q(cnt[7]), .En(ff6_ff7), .T(Clk), .Res(Res), .Pres());
+  tflipflop T0 (.Q(cnt[0]), .En(En), .T(Clk), .Res(Res), .Load(Load), .Din(cnt_in[0]));
+  tflipflop T1 (.Q(cnt[1]), .En(ff0_ff1), .T(Clk), .Res(Res), .Load(Load), .Din(cnt_in[1]));
+  tflipflop T2 (.Q(cnt[2]), .En(ff1_ff2), .T(Clk), .Res(Res), .Load(Load), .Din(cnt_in[2]));
+  tflipflop T3 (.Q(cnt[3]), .En(ff2_ff3), .T(Clk), .Res(Res), .Load(Load), .Din(cnt_in[3]));
+  tflipflop T4 (.Q(cnt[4]), .En(ff3_ff4), .T(Clk), .Res(Res), .Load(Load), .Din(cnt_in[4]));
+  tflipflop T5 (.Q(cnt[5]), .En(ff4_ff5), .T(Clk), .Res(Res), .Load(Load), .Din(cnt_in[5]));
+  tflipflop T6 (.Q(cnt[6]), .En(ff5_ff6), .T(Clk), .Res(Res), .Load(Load), .Din(cnt_in[6]));
+  tflipflop T7 (.Q(cnt[7]), .En(ff6_ff7), .T(Clk), .Res(Res), .Load(Load), .Din(cnt_in[7]));
 
 endmodule
--- count_regression.v
+++ count_regression.v
@@ -1,5 +1,8 @@
 `timescale 1ns/1ps
 
+`include "count8a.v"
+`include "count8b.v"
+
 module count_tb;
 
 	// Inputs
@@ -12,7 +15,7 @@ module count_tb;
 	integer i;
 
 	// count8b is provided by "The Architect", count8a by "The Developer Teams" 
-    count8a DUT(.CNT(CNT1_TB), .clk(clk_TB), .res(res_TB), .EN(EN_TB), .load(load_TB), .CNT_In(CNT_In_TB));
+    count8a DUT(.cnt(CNT1_TB), .Clk(clk_TB), .Res(res_TB), .En(EN_TB), .Load(load_TB), .cnt_in(CNT_In_TB));
 	count8b REF(.CNT(CNT2_TB), .clk(clk_TB), .res(res_TB), .EN(EN_TB), .load(load_TB), .CNT_In(CNT_In_TB));
 
 
